85950a7d9bba52d0b583449135d07f05b132c6e8
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2024-08-15 22:31:09 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+"""Function to query total subscribers on a given Reddit subreddit."""
 import requests
 
 def number_of_subscribers(subreddit):
+    """Function to query total subscribers on a given Reddit subreddit."""
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
     
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
