fb8e3aeb1b4073ebe4eb4fef5f017c10c1c93afc
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2024-08-15 22:58:51 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,15 +4,13 @@
 import requests
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Custom User Agent'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
+            return response.json()['data']['subscribers']
         else:
             return 0
     except Exception:
