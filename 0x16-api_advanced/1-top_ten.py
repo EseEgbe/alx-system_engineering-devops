ab3c6ae4a045816f3fcdf5bd355c0dc7627c936b
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2024-08-15 23:08:30 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,9 +3,12 @@
 import requests
 
 def top_ten(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
 
