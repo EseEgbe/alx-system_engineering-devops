4831910029329862aa87a60043b20aa789fe9e15
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2024-08-15 22:41:23 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,8 +4,6 @@ Function that queries the Reddit API and returns
 the number of subscribers for a given subreddit.
 """
 import requests
-import sys
-
 
 def number_of_subscribers(subreddit):
     """ Queries to Reddit API """
@@ -16,12 +14,10 @@ def number_of_subscribers(subreddit):
     }
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    else:
         return 0
-    dic = res.json()
-    if 'data' not in dic:
-        return 0
-    if 'subscribers' not in dic.get('data'):
-        return 0
-    return res.json()['data']['subscribers']
+
