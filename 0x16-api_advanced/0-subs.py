fe2874e2e8e6f3d8a3e8403a2e1b891321f9499d
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2024-08-15 22:42:53 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
     
-    if response.status_code == 404:
+    if response.status_code == 200:
         return response.json()['data']['subscribers']
     else:
         return 0
