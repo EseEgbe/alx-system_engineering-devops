0d729c691ce36fa4ac4e2693e30a4aa891a020ac
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2024-08-15 22:51:40 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns
-the number of subscribers for a given subreddit.
-"""
+"""Function to query total subscribers on a Subreddit."""
+
 import requests
 
 def number_of_subscribers(subreddit):
-    """ Queries to Reddit API """
+    """Return the total number of subscribers on a given subreddit."""
     headers = {'User-Agent': 'CustomUserAgent/1.0'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, headers=headers, allow_redirects=False)
     
-    if response.status_code == 200:
-        return response.json()['data']['subscribers']
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception:
         return 0
